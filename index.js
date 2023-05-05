5c06a6961b2945ae6cc48ea88ee634d0355d02c5
Jess Leyva <jessica.leyvat@yahoo.com>
2023-05-05 13:35:18 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(uName,uEmail){
+    const postObject = {
+        method :"POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body : JSON.stringify({
+            name : uName,
+            email : uEmail,
+        })
+    }
+    return fetch("http://localhost:3000/users",postObject)
+    .then((resp) => resp.json())
+    .then((e) => document.body.append(e.id))
+    .catch(function (error){
+       document.body.append('Unauthorized Access')
+    })
+}
+
+
+submitData("Sam","sam@sam.com")
